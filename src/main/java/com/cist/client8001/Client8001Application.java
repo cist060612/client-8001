e2752c216c2fbe77136cd26d9bf04a54ddf1f6dd
zah <cist@qq.com>
2022-04-11 22:11:25 +0800

    test

--- src/main/java/com/cist/client8001/Client8001Application.java
+++ src/main/java/com/cist/client8001/Client8001Application.java
@@ -10,6 +10,7 @@ public class Client8001Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Client8001Application.class, args);
+        System.out.println("hello");
     }
 
 }
