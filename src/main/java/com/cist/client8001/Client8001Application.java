05ffd494c7d3de605dc76a99b766c9cdaffb8cbc
zah <cist@qq.com>
2022-04-11 22:10:23 +0800

    test

---
+++ src/main/java/com/cist/client8001/Client8001Application.java
@@ -0,0 +1,15 @@
+package com.cist.client8001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Client8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client8001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cist/client8001/Client8001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cist.client8001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Client8001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
